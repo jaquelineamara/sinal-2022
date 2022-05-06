69cfc5b526aa601c3918a134990b44dec485bca2
micheli <miihrusso2017@gmail.com>
2022-05-06 20:36:56 -0300

    finaliza tarefa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numero;
+
+Console.Write("digite um Número: ");
+numero = Convert.ToInt32(Console.ReadLine ());
+
+if (numero < 0 )
+
+{
+    Console.WriteLine("Negativo");
+}
+
+else if (numero > 0)
+{
+    Console.WriteLine("Positivo");
+}
+
+else
+{
+    Console.WriteLine("Zero");
+}
